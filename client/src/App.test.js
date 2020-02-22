33621d17268b1c1ceece655e0d0878315964d266
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-02-21 17:42:33 -0800

    3 tests passing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import render from '@testing-library/react';
 import App from './App';
+import DarkMode from './components/DarkMode';
+import Players from './components/Players';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('Dark mode renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<DarkMode />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+test('Players renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Players />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
